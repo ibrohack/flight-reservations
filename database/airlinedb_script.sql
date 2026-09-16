d060d963b863523fdbe66d39c89648bd8c4a514f
Lichi <lordlichi2006@gmail.com>
2026-09-16 09:58:48 +0200

    Spell checked SQL script

--- database/airlinedb_script.sql
+++ database/airlinedb_script.sql
@@ -9,15 +9,15 @@ CREATE TABLE AIRLINE (
     iata_code VARCHAR (10)
 );
 
-CREATE TABLE COSTUMER (
-	costumer_id INT AUTO_INCREMENT PRIMARY KEY,
-    costumerName VARCHAR (100),
+CREATE TABLE CUSTOMER (
+	customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customerName VARCHAR (100),
     email VARCHAR (20), 
     phoneNumber VARCHAR (10),
     route VARCHAR (100)
 );
 
-CREATE TABLE FLIGTH (
+CREATE TABLE FLIGHT (
     flight_id INT AUTO_INCREMENT PRIMARY KEY,
     origin VARCHAR (100),
     destination VARCHAR (100),
@@ -26,7 +26,7 @@ CREATE TABLE FLIGTH (
     travel_class ENUM('ECONOMY', 'BUSINESS', 'FIRST'),
     airline_id INT,
 	FOREIGN KEY (airline_id) REFERENCES airline (airline_id)
-    );
+);
     
 CREATE TABLE BOOK (
 	customer_id INT NOT NULL,
